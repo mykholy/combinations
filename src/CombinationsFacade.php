b4e708037104ad08c96e6aa3eb7fd53208096bec
mykholy <mykholys30@gmail.com>
2024-04-09 19:01:13 +0200

    first commit

---
+++ src/CombinationsFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mykholy\Combinations;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Laracon21\Combinations\Skeleton\SkeletonClass
+ */
+class CombinationsFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'combinations';
+    }
+}
